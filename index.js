601c48b12d01ec003af0faaae6e1d251c13decf0
Emtrixx <jesseguenzl@t-online.de>
2021-03-22 18:09:24 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen("4000", () => {
+    console.log("Server gestartet");
+})
