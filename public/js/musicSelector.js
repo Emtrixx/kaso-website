93d1b549295850867e2eab46a76ca6ac603d6c76
Jesse Günzl <20597157+Emtrixx@users.noreply.github.com>
2021-03-29 21:46:05 +0200

    added switching soundcloud players

--- public/js/musicSelector.js
+++ public/js/musicSelector.js
@@ -1,12 +1,12 @@
 const kacheln = document.querySelectorAll(".kachel");
 const scplayer = document.querySelector(".soundcloud");
 
-console.log(scplayer);
+kacheln[0].addEventListener("click", function () {
+    scplayer.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/934401364&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
 
+})
 
-for(kachel of kacheln) {
-    kachel.addEventListener("click", function() {
-        console.log(scplayer.src)
-    })
-}
+kacheln[1].addEventListener("click", function () {
+    scplayer.src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/892024321&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+})
 
