9a1558dcdb6f5276cc6d79299726a68c5bcbbc4b
Jesse Günzl <20597157+Emtrixx@users.noreply.github.com>
2021-03-31 18:38:04 +0200

    add error page and minor cleanup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ExpressError = require('./utils/ExpressError');
 
 app.use(express.static(path.join(__dirname,"public")));
 
@@ -8,6 +9,17 @@ app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+
+app.all('*', (req,res,next) => {
+    // res.status(404).render("notFound");
+    next(new ExpressError('Not Found', 404));
+})
+
+app.use((err,req,res,next) => {
+    const { message = 'Something went wrong', statusCode = 500, stack} = err;
+    res.status(statusCode).render('error', {message, stack});
 })
 
 app.listen("4000", () => {
